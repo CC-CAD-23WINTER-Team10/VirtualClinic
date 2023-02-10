b1420e00723a7738f10838940741acf768c8cb94
badpolymer <roans_policy.07@icloud.com>
2023-02-10 13:04:52 -0500

    Turn On the local Camara without voice

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const server = require(`http`).Server(app);
 
 app.set(`view engine`, `ejs`);
-
+app.use(express.static(`public`));
 
 /*
 * Routes
 */
 app.get(`/`, (req, res) => {
-    res.status(200).send(`Hello World`);
+    //res.status(200).send(`Hello World`);
+    res.render(`test2`);
 });
 
 
---
+++ public/js/test2.js
@@ -0,0 +1,67 @@
+/* 
+* Client Side JS
+*/
+
+let myVideoStream; //localCamera
+let remoteVideoStream; //Camera From others
+let peerConnection;
+
+const servers = {
+    //STUN Servers
+    iceServers: [
+        {
+            urls: [`stun:stun1.l.google.com:19302`, `stun:stun2.l.google.com:19302`]
+        }
+    ]
+};
+
+/**
+ * 
+ * @param {boolean} audio Set audio to true in production or when testing between two computer. Set audio to false when single computer testing.
+ */
+async function getLocalStream(audio) {
+    myVideoStream = await navigator.mediaDevices.getUserMedia({
+        audio: audio,  //Set false for single computer testing
+        //peerIdentity: ``,
+        //preferCurrentTab: true/false,
+        video: true
+    })
+
+    document.getElementById(`user-1`).srcObject = myVideoStream;
+}
+
+/**
+ * function Description
+ */
+async function sendVideoCallRequest() {
+    peerConnection = new RTCPeerConnection(servers);
+
+    remoteVideoStream = new MediaStream();
+
+    document.getElementById(`user-2`).srcObject = remoteVideoStream;
+
+    
+
+    /**
+     * Add Local Tracks to peerConnection
+     */
+    let localTracks = myVideoStream.getTracks();
+
+    localTracks.forEach( track => {
+        
+        peerConnection.addTrack(track,myVideoStream);
+        
+    });
+
+    /**
+     * Set Offer
+     */
+    let offer = await peerConnections[0].createOffer();
+
+    await peerConnection.setLocalDescription(offer);
+}
+
+
+document.getElementById(`start`).onclick = function() {
+    getLocalStream(false);
+};
