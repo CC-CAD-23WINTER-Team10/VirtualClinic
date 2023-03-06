6aa9a76067cf473c9b6652f77ace949e82591d20
badpolymer <roans_policy.07@icloud.com>
2023-03-06 00:00:33 -0500

    add home page js

---
+++ public/js/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Login Page 
+ */
+const socket = io(`/`);
+var loginForm = document.getElementById(`loginForm`) as HTMLFormElement;
+var submitButton = document.getElementById(`submit`) as HTMLAnchorElement;
+
+submitButton.onclick = function() {
+    console.log(`SUBMIT BUTTON IS CLICKED.`);
+    loginForm.submit();
+};
+
+
+
+
+
+/**
+ * Socket.on
+ */
+socket.on("connect", () => {
+    console.log(`CONNECTED WITH SERVER. YOUR ID: `, socket.id);
+});
+socket.on("disconnect", () => {
+    console.log(`DISCONNECTED WITH SERVER.`);
+});
