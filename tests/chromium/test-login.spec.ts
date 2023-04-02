accab4fb58a2dcf36aadf82a0d0a84e6610bf7b5
Ormigcd <ormma@engineer.com>
2023-04-02 13:18:33 -0400

    login Automate testing in Chromium and Firefox

---
+++ tests/chromium/test-login.spec.ts
@@ -0,0 +1,89 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test('test', async () => {
+  const browser = await chromium.launch();  // Or 'chromium' or 'webkit'.
+  // Create a new incognito browser context.
+  const context = await browser.newContext();
+  // Create a new page in a pristine context.
+  const page = await context.newPage();
+
+  await page.goto('http://localhost/');
+  //If user clicks on Dashboard then should stay in home page
+  await page.getByRole('link', { name: 'Dashboard' }).click();
+  await expect(page).toHaveURL('http://localhost/');
+
+  // if user clicks on Home then should stay in home page
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page).toHaveURL('http://localhost/');
+
+  // if user submits blank or empty data then NO access is allowed to dashboard
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters only a wrong user name then NO access allowed to dashboard
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('admin');
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters only an wrong password then NO access allowed to dashboard
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('123456');
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters wrong username and password then NO access allowed to dashboard
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('admin');
+  await page.getByLabel('Username').press('Tab');
+  await page.getByLabel('Password').fill('admin');
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters correct username but no password then NO access allowed to dashboard
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('001');
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters no username and correct password then NO access allowed to dashboard
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('001');
+  await page.getByText('Submit').click();
+  await expect(page.getByText('You entered a wrong username or password.')).toHaveText('You entered a wrong username or password.');
+
+  // if user enters a correct user name and password then access to dashboard is conceded
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('001');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('001');
+  await page.getByText('Submit').click();
+  await expect(page.locator('div').filter({ hasText: 'Andrew Dip' }).nth(2)).toHaveText("Andrew Dip");
+
+  // if user enters a correct user name and password then access to dashboard is conceded
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('002');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('002');
+  await page.getByText('Submit').click();
+  await expect(page.locator('div').filter({ hasText: 'Anderson Delfino' }).nth(2)).toHaveText("Anderson Delfino");
+
+  // if user enters a correct user name and password then access to dashboard is conceded
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('003');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('003');
+  await page.getByText('Submit').click();
+  await expect(page.locator('div').filter({ hasText: 'David Smith' }).nth(2)).toHaveText("David Smith");
+
+  // if user enters a correct user name and password then access to dashboard is conceded
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('004');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('004');
+  await page.getByText('Submit').click();
+  await expect(page.locator('div').filter({ hasText: 'May Smith' }).nth(2)).toHaveText("May Smith");
+});
---
+++ tests/firefox/test-Users_Logged.spec.ts
@@ -0,0 +1,87 @@
+import { test, expect, firefox } from '@playwright/test';
+
+//NOte: This test may fail by excecution time --so setTimeout was extended to 60seg in playwright.config.ts and setTimeout to 120seg.
+
+// Checks physician1 loged in 
+test('test', async () => {
+  test.setTimeout(120000);
+  const browser1 = await firefox.launch();  // Or 'chromium' or 'webkit'.
+  // Create a new incognito browser context.
+  const context1 = await browser1.newContext();
+  // Create a new page in a pristine context.
+  const page1 = await context1.newPage();
+
+  await page1.goto('http://localhost/');
+  await page1.getByLabel('Username').click();
+  await page1.getByLabel('Username').fill('001');
+  await page1.getByLabel('Username').press('Tab');
+  await page1.getByLabel('Password').fill('001');
+  await page1.getByText('Submit').click();
+  await expect(page1.locator('div').filter({ hasText: 'Andrew Dip' }).nth(2)).toHaveText("Andrew Dip");
+
+  // ***Second user join the app
+  const browser2 = await firefox.launch();  // Or 'chromium' or 'webkit'.
+  // Create a new incognito browser context.
+  const context2 = await browser2.newContext();
+  // Create a new page in a pristine context.
+  const page2 = await context2.newPage();
+
+  await page2.goto('http://localhost/');
+  await page2.getByLabel('Username').click();
+  await page2.getByLabel('Username').fill('002');
+  await page2.getByLabel('Username').press('Tab');
+  await page2.getByLabel('Password').fill('002');
+  await page2.getByText('Submit').click();
+  await expect(page2.locator('div').filter({ hasText: 'Anderson Delfino' }).nth(2)).toHaveText("Anderson Delfino");
+  
+  await expect(page1.locator('div').filter({ hasText: 'Anderson Delfino' }).nth(3)).toHaveText("Anderson Delfino");
+  
+  await expect(page2.locator('div').filter({ hasText: 'Andrew Dip' }).nth(3)).toHaveText("Andrew Dip");
+  
+  // ***Third user join the app
+  const browser3 = await firefox.launch();  // Or 'chromium' or 'webkit'.
+  // Create a new incognito browser context.
+  const context3 = await browser3.newContext();
+  // Create a new page in a pristine context.
+  // Create a new page in a pristine context.
+  const page3 = await context3.newPage();
+
+  await page3.goto('http://localhost/');
+  await page3.getByLabel('Username').click();
+  await page3.getByLabel('Username').fill('003');
+  await page3.getByLabel('Username').press('Tab');
+  await page3.getByLabel('Password').fill('003');
+  await page3.getByText('Submit').click();
+  await expect(page3.locator('div').filter({ hasText: 'David Smith' }).nth(2)).toHaveText("David Smith");
+
+  await expect(page1.locator('div').filter({ hasText: 'David Smith' }).nth(3)).toHaveText("David Smith");
+  await expect(page2.locator('div').filter({ hasText: 'David Smith' }).nth(3)).toHaveText("David Smith");
+    
+  await expect(page3.locator('div').filter({ hasText: 'Andrew Dip' }).nth(3)).toHaveText("Andrew Dip");
+  await expect(page3.locator('div').filter({ hasText: 'Anderson Delfino' }).nth(3)).toHaveText("Anderson Delfino");
+
+
+  const browser4 = await firefox.launch();  // Or 'chromium' or 'webkit'.
+  // Create a new incognito browser context.
+  const context4 = await browser4.newContext();
+  // Create a new page in a pristine context.
+  const page4 = await context4.newPage();
+
+  await page4.goto('http://localhost/');
+  await page4.getByLabel('Username').click();
+  await page4.getByLabel('Username').fill('004');
+  await page4.getByLabel('Username').press('Tab');
+  await page4.getByLabel('Password').fill('004');
+  await page4.getByText('Submit').click();
+  await expect(page4.locator('div').filter({ hasText: 'May Smith' }).nth(2)).toHaveText("May Smith");
+
+  await expect(page1.locator('div').filter({ hasText: 'May Smith' }).nth(3)).toHaveText("May Smith");
+  await expect(page2.locator('div').filter({ hasText: 'May Smith' }).nth(3)).toHaveText("May Smith");
+  
+  await expect(page4.locator('div').filter({ hasText: 'Andrew Dip' }).nth(3)).toHaveText("Andrew Dip");
+  await expect(page4.locator('div').filter({ hasText: 'Anderson Delfino' }).nth(3)).toHaveText("Anderson Delfino");
+  // Gracefully close up everything
+  //await context.close();
+  //await browser.close();
+});
+
